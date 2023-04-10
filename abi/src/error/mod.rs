cd017abff20f82e218da2f2fbae7a9f1aac14425
liwei <liweilijie@gmail.com>
2023-04-10 16:53:22 +0800

    use regex to parse the database error about reservation reserve conflict.

---
+++ abi/src/error/mod.rs
@@ -0,0 +1,49 @@
+mod conflict;
+
+use sqlx::postgres::PgDatabaseError;
+use thiserror::Error;
+
+pub use conflict::{ReservationConflictInfo, ReservationWindow};
+
+#[derive(Error, Debug)]
+pub enum Error {
+    // #[error("data store disconnected")]
+    // Disconnect(#[from] io::Error),
+    // #[error("the data for key `{0}` is not available")]
+    // Redaction(String),
+    // #[error("invalid header (expected {expected:?}, found {found:?})")]
+    // InvalidHeader { expected: String, found: String },
+    #[error("Database error")]
+    DbError(sqlx::Error),
+    #[error("Invalid start or end time for the reservation")]
+    InvalidTime,
+
+    #[error("Conflict reservation")]
+    ConflictReservation(ReservationConflictInfo),
+
+    #[error("Invalid user id: {0}")]
+    InvalidUserId(String),
+
+    #[error("Invalid resource id: {0}")]
+    InvalidResourceId(String),
+
+    #[error("unknown error")]
+    Unknown,
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(e: sqlx::Error) -> Self {
+        match e {
+            sqlx::Error::Database(e) => {
+                let err: &PgDatabaseError = e.downcast_ref();
+                match (err.code(), err.schema(), err.table()) {
+                    ("23P01", Some("rsvp"), Some("reservations")) => {
+                        Error::ConflictReservation(err.detail().unwrap().parse().unwrap())
+                    }
+                    _ => Error::DbError(sqlx::Error::Database(e)),
+                }
+            }
+            _ => Error::DbError(e),
+        }
+    }
+}
