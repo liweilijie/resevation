4786be332ec0e5f4b13ca98a5c4180fd7769a401
liwei <liweilijie@gmail.com>
2023-04-18 10:52:52 +0800

    use the sqlx-mock-db-tester crate to fixed the rpc service.

--- service/src/service.rs
+++ service/src/service.rs
@@ -89,11 +89,9 @@ mod tests {
     use super::*;
     use abi::Reservation;
     use lazy_static::lazy_static;
-    use sqlx::types::Uuid;
-    use sqlx::{Connection, Executor, PgConnection};
+    use sqlx_mock_db_tester::TestPg;
     use std::ops::Deref;
-    use std::sync::Arc;
-    use std::thread;
+    use std::path::Path;
     use tokio::runtime::Runtime;
 
     lazy_static! {
@@ -101,7 +99,9 @@ mod tests {
     }
 
     struct TestConfig {
-        config: Arc<Config>,
+        #[allow(dead_code)]
+        tdb: TestPg,
+        pub config: Config,
     }
 
     impl Deref for TestConfig {
@@ -114,60 +114,12 @@ mod tests {
 
     impl TestConfig {
         pub fn new() -> Self {
-            let mut config = Config::load("../service/fixtures/config.yml").unwrap();
-            let uuid = Uuid::new_v4();
-            let dbname = format!("test_{}", uuid);
-            config.db.dbname = dbname.clone();
+            let mut config = Config::load("fixtures/config.yml").unwrap();
+            println!("{}", config.db.server_url());
+            let tdb = TestPg::new(config.db.server_url(), Path::new("../migrations"));
+            config.db.dbname = tdb.dbname.clone();
 
-            let server_url = config.db.server_url();
-            let url = config.db.url();
-
-            // create database dbname
-            thread::spawn(move || {
-                TEST_RT.block_on(async move {
-                    // use server url to create database
-                    let mut conn = PgConnection::connect(&server_url).await.unwrap();
-                    conn.execute(format!(r#"CREATE DATABASE "{}""#, dbname).as_str())
-                        .await
-                        .unwrap();
-
-                    // now connect to test database for migration
-                    let mut conn = PgConnection::connect(&url).await.unwrap();
-                    sqlx::migrate!("../migrations")
-                        .run(&mut conn)
-                        .await
-                        .unwrap();
-                });
-            })
-            .join()
-            .expect("failed to create database.");
-
-            Self {
-                config: Arc::new(config),
-            }
-        }
-    }
-
-    impl Drop for TestConfig {
-        fn drop(&mut self) {
-            let server_url = self.db.server_url();
-            let dbname = self.config.db.dbname.clone();
-            thread::spawn(move || {
-                TEST_RT.block_on(async move {
-                    let mut conn = sqlx::PgConnection::connect(&server_url).await.unwrap();
-                    // terminate existing connections
-                    sqlx::query(&format!(r#"SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = '{}'"#, dbname))
-                        .execute(&mut conn)
-                        .await
-                        .expect("Terminate all other connections");
-
-                    conn.execute(format!(r#"DROP DATABASE "{}""#, dbname).as_str())
-                        .await
-                        .expect("Error while querying the drop database");
-                });
-            })
-                .join()
-                .expect("failed to drop database");
+            Self { tdb, config }
         }
     }
 
